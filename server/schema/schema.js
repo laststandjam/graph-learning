27d6467f39642120db27e33602d9bee11df8c160
ihavehands <zaneshook@gmail.com>
2020-04-12 15:34:10 -0700

    schema

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,5 +1,7 @@
 const graphql = require("graphql");
 const _ = require("lodash");
+const Book = require("../models/book")
+const Author = require("../models/author")
 
 const {
   GraphQLObjectType,
@@ -10,18 +12,7 @@ const {
   GraphQLList
 } = graphql;
 
-//dummy data
-var books = [
-  { name: "Name of the Wind", genre: "Fantasy", id: "1", authorId: "1" },
-  { name: "The Final Empire", genre: "Fantasy", id: "2", authorId:"2" },
-  { name: "The long earth", genre: "Sci-Fi", id: "3", authorId:"1" },
-  { name: "The Hero of Ages", genre: "Fantasy", id:"4", authorId: "2"}
-];
-const authors = [
-  { name: "Patrick Rothfuss", age: 44, id: "1" },
-  { name: "Brandon Sanderson", age: 42, id: "2" },
-  { name: "Terry Pratchett", age: 66, id: "3" }
-];
+
 const BookType = new GraphQLObjectType({
   name: "Book",
   fields: () => ({
