782b21c8e168f7dd82af0285c20817528d8c3e2d
ihavehands <zaneshook@gmail.com>
2020-04-13 09:48:03 -0700

    update resolve

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -22,8 +22,7 @@ const BookType = new GraphQLObjectType({
     author: {
         type: AuthorType,
         resolve(parent, args){
-            console.log(parent)
-            return _.find(authors,{id: parent.authorId})
+           return Author.findById(parent.authorId)
         }
     }
   })
@@ -37,7 +36,7 @@ const AuthorType = new GraphQLObjectType({
     books:{
       type: new  GraphQLList(BookType),
       resolve(parent, args){
-        return _.filter(books, {authorId: parent.id})
+        return Book.find({ authorId: parent.id}) 
       }
     }
   })
@@ -52,26 +51,26 @@ const RootQuery = new GraphQLObjectType({
       resolve(parent, args) {
         args.id;
         //code to get data from db/api
-        return _.find(books, { id: args.id });
+        return Book.findById(args.id)
       }
     }, 
     author:{
         type:AuthorType,
         args: {id:{type:GraphQLID}},
         resolve(parent, args){
-            return _.find(authors, {id:args.id})
+            return Author.findById(args.id)
         }
      },
      books:{
        type: new GraphQLList(BookType),
        resolve(parent, args){
-          return books
+          return Book.find({})
        }
      },
      authors:{
        type: new GraphQLList(AuthorType),
        resolve(parent, args){
-         return authors
+         return Author.find({})
        }
      }
   }
