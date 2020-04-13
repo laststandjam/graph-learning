ba5d069c8386ea947b985faa50f1a17acbf560d9
ihavehands <zaneshook@gmail.com>
2020-04-13 09:09:52 -0700

    mutations

--- server/app.js
+++ server/app.js
@@ -4,7 +4,7 @@ const schema = require('./schema/schema')
 const mongoose = require('mongoose')
 
 const app = express()
-mongoose.connect("mongodb+srv://Zane:F1nuc0ne@book-club-project-fmquq.azure.mongodb.net/test?retryWrites=true&w=majority", {useNewUrlParser:true, useUnifiedTopology: true} 
+mongoose.connect("mongodb+srv://Zane:F1nuc0ne@book-club-project-fmquq.azure.mongodb.net/graphql?retryWrites=true&w=majority", {useNewUrlParser:true, useUnifiedTopology: true} 
 ).then(() => console.log("Connected to MongoDB Atlas"))
 .catch(err => console.log("Error: ", err.message));
 
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -6,7 +6,7 @@ const Author = require("../models/author")
 const {
   GraphQLObjectType,
   GraphQLString,
-  GraphQLSchema,
+  GraphQLSchema, 
   GraphQLID,
   GraphQLInt,
   GraphQLList
@@ -77,6 +77,45 @@ const RootQuery = new GraphQLObjectType({
   }
 });
 
+const Mutation = new GraphQLObjectType ({
+  name: "Mutation",
+  fields:{
+    addAuthor:{
+      type: AuthorType,
+      args:{
+        name:{
+          type:GraphQLString
+          },
+          age:{type:GraphQLInt}
+      },
+      resolve(parent, args){
+        let author = new Author({
+          name: args.name,
+          age: args.age
+        })
+        return author.save()
+      }
+    },
+    addBook:{
+      type:BookType,
+      args: {
+        name:{type: GraphQLString},
+        genre: { type: GraphQLString},
+        authorId: {type: GraphQLID}
+      },
+      resolve(parent, args){
+        let book = new Book({
+          name: args.name,
+          genre: args.genre,
+          authorId: args.authorId
+        })
+        return book.save()
+      }
+    }
+  }
+})
+
 module.exports = new GraphQLSchema({
-  query: RootQuery
+  query: RootQuery,
+  mutation: Mutation
 });
