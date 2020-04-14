6ede2b73beeeca24e2431d45d40ae9d86bdac488
ihavehands <zaneshook@gmail.com>
2020-04-13 17:34:55 -0700

    booklist-component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-
+import BookList from './components/BookList'
 
 function App() {
   return (
     <div className="App">
      <h1>Zane's & Genesis' Reading List</h1>
+     <BookList/>
     </div>
   );
 }
---
+++ client/src/components/BookList.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+const BookList = () => {
+  return (
+    <div className="App">
+      <ul id="book-list"> </ul>
+      <li>Book name</li>
+    </div>
+  );
+};
+
+export default BookList;
