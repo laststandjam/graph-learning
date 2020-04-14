e086a6b6976525a661bb56a8b542c5fee7c053e6
ihavehands <zaneshook@gmail.com>
2020-04-14 14:08:24 -0700

    playing with apoolo

--- client/src/components/BookList.jsx
+++ client/src/components/BookList.jsx
@@ -1,13 +1,26 @@
 import React from "react";
+import { gql } from "apollo-boost";
+import { graphql } from "react-apollo"
 
+const getBooksQuery = gql`
+  {
+    books {
+      name
+      id
+    }
+  }
+`;
 
 const BookList = () => {
+  
   return (
-    <div className="App">
+   
+    <div className="App"> 
+    {console.log(getBooksQuery)}
       <ul id="book-list"> </ul>
       <li>Book name</li>
     </div>
   );
 };
 
-export default BookList;
+export default graphql(getBooksQuery)(BookList);
--- server/app.js
+++ server/app.js
@@ -2,8 +2,13 @@ const express = require('express')
 const graphqlHTTP = require('express-graphql')
 const schema = require('./schema/schema')
 const mongoose = require('mongoose')
+const cors = require("cors")
 
 const app = express()
+
+//cors-origin requests
+app.use(cors())
+
 mongoose.connect("mongodb+srv://Zane:F1nuc0ne@book-club-project-fmquq.azure.mongodb.net/graphql?retryWrites=true&w=majority", {useNewUrlParser:true, useUnifiedTopology: true} 
 ).then(() => console.log("Connected to MongoDB Atlas"))
 .catch(err => console.log("Error: ", err.message));
